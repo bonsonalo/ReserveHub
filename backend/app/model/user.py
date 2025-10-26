1029496a5bb126101198703ca16db4789c283e36
bonsonalo <bonson2468@gmail.com>
2025-10-27 03:41:25 +0800

    feat: add relationship for booking and booking event

--- backend/app/model/user.py
+++ backend/app/model/user.py
@@ -3,6 +3,9 @@ from backend.app.core.database import Base
 import uuid
 from sqlalchemy import UUID, String, Integer, Boolean, ForeignKey, TIMESTAMP, func
 from typing import Optional
+
+from backend.app.model.booking import Booking
+from backend.app.model.booking_event import BookingEvent
 from .associations import user_roles
 
 
@@ -52,4 +55,12 @@ class User(Base):
     roles: Mapped["list[Role]"]= relationship(
         secondary= user_roles,
         back_populates= "users"
+    )
+    bookings: Mapped[list[Booking]]= relationship(
+        back_populates= "users",
+        cascade= "save-update"
+    )
+    booking_event: Mapped[list["BookingEvent"]]= relationship(
+        back_populates= "user",
+        cascade= "all, delete-orphan"
     )
