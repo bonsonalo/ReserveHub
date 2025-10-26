82d0881a84a255da3a5c3d625117feb504129903
bonsonalo <bonson2468@gmail.com>
2025-10-27 03:37:49 +0800

    feat: add booking db model

---
+++ backend/app/model/booking.py
@@ -0,0 +1,95 @@
+from sqlalchemy import Boolean, Integer, ForeignKey, String, func, UUID, text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+import uuid
+from backend.app.core.database import Base
+from backend.app.model.booking_event import BookingEvent
+from backend.app.model.email import Email
+from .resource import Resource
+from .user import User
+from sqlalchemy.dialects.postgresql import TSTZRANGE, JSONB, TIMESTAMP
+from datetime import datetime
+from typing import Any
+
+
+
+
+class Booking(Base):
+    __tablename__= "bookings"
+
+    id: Mapped[uuid.UUID]= mapped_column(
+        UUID(as_uuid= True),
+        primary_key=True,
+        server_default= uuid.uuid4
+    )
+    resource_id: Mapped[uuid.UUID]= mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey("resources.id", ondelete="SET NULL"),
+        nullable= True
+    )
+    user_id: Mapped[uuid.UUID]= mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey("User.id", ondelete= "SET NULL")
+    )
+    status: Mapped[str]= mapped_column(
+        String,
+        nullable= False,
+        default= "pending",
+        server_default= text("'pending'")
+    )
+    time_range: Mapped[tuple[datetime, datetime]]= mapped_column(
+        TSTZRANGE,
+        nullable= False
+    )
+    attendees: Mapped[int] = mapped_column(
+        Integer,
+        default= 1
+    )
+    data: Mapped[dict[str, Any]]= mapped_column(
+        JSONB,
+        server_default= text('{}::jsonb')   
+    )
+    created_by: Mapped[uuid.UUID]= mapped_column(
+        UUID(as_uuid= True),
+        ForeignKey("users.id", ondelete= "SET NULL"),
+        server_default= uuid.uuid4
+    )
+    created_at: Mapped[datetime]= mapped_column(
+        TIMESTAMP(timezone= True),
+        server_default= func.now()
+    )
+    updated_at: Mapped[datetime]= mapped_column(
+        TIMESTAMP(timezone= True),
+        server_default= func.now(),
+        onupdate= func.now()
+    )
+    canceled_at: Mapped[datetime]= mapped_column(
+        TIMESTAMP(timezone= True)
+    )
+    is_recurring: Mapped[bool]= mapped_column(
+        Boolean,
+        default= False
+    )
+    recurrence_rule: Mapped[str]= mapped_column(
+        String
+    )
+
+
+
+
+    resources: Mapped["Resource | None"]= relationship(
+        back_populates= "bookings"
+    )
+    users: Mapped["User | None"]= relationship(
+        back_populates= "bookings"
+    )
+    user: Mapped["User | None"]= relationship(
+        back_populates= "booking"
+    )
+    booking_event: Mapped[list["BookingEvent"]]= relationship(
+        back_populates= "booking",
+        cascade= "all, delete-orphan"
+    )
+    emails: Mapped[list["Email"]]= relationship(
+        back_populates= "bookings",
+        cascade= "all, delete-orphan"
+    )
