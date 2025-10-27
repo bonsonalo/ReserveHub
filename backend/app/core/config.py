a5132fcb8e4b3fb6308537d147989eec94281717
bonsonalo <bonson2468@gmail.com>
2025-10-28 02:26:39 +0800

    feat: add db_dependency

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,22 @@
+from fastapi import Depends
+from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+
+
 from backend.app.core.database import AsyncSessionLocal
+
+
+
+
+
+
 
 
 async def get_db():
     async with AsyncSessionLocal() as db:
         yield db
+
+    
+db_dependency= Annotated[AsyncSession, Depends(get_db)]
