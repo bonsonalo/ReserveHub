fa330fb56b81a12dee1922e3b4e12eb8d158a0ff
bonsonalo <bonson2468@gmail.com>
2025-10-30 02:15:38 +0800

    feat: add code to promote use to higher role and refresh token

--- backend/app/api/v1/endpoints/auth.py
+++ backend/app/api/v1/endpoints/auth.py
@@ -5,10 +5,12 @@ from starlette import status
 from passlib.context import CryptContext
 from typing import Annotated
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import ExpiredSignatureError, JWTError
+
 
 from backend.app.schema.auth_schema import CreateUserRequest, Token
-from backend.app.core.config import db_dependency
-from backend.app.service.auth_service import create_access_token, create_user_service, authenticate_user
+from backend.app.core.config import db_dependency, user_dependency, admin_dependency, superadmin_dependency
+from backend.app.service.auth_service import create_access_token, create_user_service, authenticate_user, promote_user_service, refresh_access_token_service
 from backend.app.utils.password_strength import validate_password_strength
 from backend.app.model.user import User
 from backend.app.core.logger import logger
@@ -47,6 +49,28 @@ async def login_for_access_token(user_form: Annotated[OAuth2PasswordRequestForm,
 
      return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
 
+# to upgrade user
+
+@router.put("/promote/{user_id}")
+async def promote_user(user_id: int, new_role: str, db: db_dependency, current_user: superadmin_dependency):
+     updated= await promote_user_service(user_id, new_role, db, current_user)
+     if not updated:
+          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "user not found")
+     return updated
+
+
+# refresh
+
+@router.post("/refresh")
+async def refresh_access_token(refresh_token: str):
+     try:
+          return refresh_access_token_service(refresh_token)
+     except ExpiredSignatureError:
+          raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="refresh token expired")
+     except JWTError:
+          raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail= "invalid refresh token")
+
+     
 
 
 
