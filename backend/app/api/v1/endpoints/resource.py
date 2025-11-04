ecf2c296125cb55d69d16d624f24589e7d1abee8
bonsonalo <bonson2468@gmail.com>
2025-11-04 12:53:21 +0800

    feat: add endpoint to update, create, get by id

--- backend/app/api/v1/endpoints/resource.py
+++ backend/app/api/v1/endpoints/resource.py
@@ -6,9 +6,9 @@ from starlette import status
 from backend.app.model.resource import Resource
 from backend.app.core.config import user_dependency, admin_dependency, superadmin_dependency, db_dependency
 from backend.app.service.auth_service import authenticate_user
-from backend.app.service.resource_service import get_all_resources_service
+from backend.app.service.resource_service import create_resource_service, get_all_resources_service, get_resource_by_id_service
 from backend.app.core.logger import logger
-
+from backend.app.schema import resource_schema
 
 
 
@@ -33,6 +33,7 @@ async def get_all_resources(db: db_dependency,
                             resource_type: Optional[str]= Query(None),
                             available: Optional[str]= Query(None)                           
                             ):
+    authenticate_user(current_user)
     try:
         return await get_all_resources_service(db,
                                                sort_by,
@@ -50,3 +51,41 @@ async def get_all_resources(db: db_dependency,
     except AttributeError as e:
         logger.error(str(e))
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= str(e))
+    
+
+# create resource
+@router.post("/create", status_code=status.HTTP_201_CREATED)
+async def create_resource(info: resource_schema.Resource, current_user: admin_dependency, db: db_dependency):
+    authenticate_user(current_user)
+    try:
+        return await create_resource_service(info, db)
+    except ValueError as e:
+        logger.error(str(e))
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= str(e))
+    
+
+# get resource by id
+@router.get("/get/{id}", status_code=status.HTTP_200_OK)
+async def get_resource_by_id(id: UUID, current_user: user_dependency, db: db_dependency):
+    authenticate_user(current_user)
+    try:
+        return await get_resource_by_id_service(id, db)
+    except ValueError as e:
+        logger.error(str(e))
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= str(e))
+    
+
+
+# update resource
+@router.patch("/update/{id}")
+async def update_resource(id: int, updated_to: resource_schema.UpdateResource, current_user: admin_dependency, db: db_dependency):
+    authenticate_user(current_user)
+    try:
+        return await update_resource(id,updated_to, db)
+    except Exception as e:
+        logger.error(str(e))
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= str(e))
+    
+
+# DELETE /api/v1/resources/{id}
+
