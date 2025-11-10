c6b3cce129a7453945080e4c4fa72dbca5588b2f
bonsonalo <bonson2468@gmail.com>
2025-11-11 07:21:37 +0800

    feat: import all models in models init files to not cause error for when I am not writing  specific endpoint

---
+++ backend/app/model/__init__.py
@@ -0,0 +1,20 @@
+
+
+
+from backend.app.model.user import User
+from backend.app.model.resource import Resource
+from backend.app.model.resource_availability import ResourceAvailability
+from backend.app.model.email import Email
+from backend.app.model.booking import Booking
+from backend.app.model.booking_event import BookingEvent
+
+
+
+__all__= [
+    "User",
+    "Resource",
+    "ResourceAvailability",
+    "Email",
+    "Booking",
+    "BookingEvent"
+]
