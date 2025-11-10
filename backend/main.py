a8fee926aec8a651cd7bd0114b3bc6ba821bd7e1
bonsonalo <bonson2468@gmail.com>
2025-11-11 07:13:24 +0800

    refactor: fix the initiation of main.py by using asynccontextmanager

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,14 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager 
 
 from backend.app.core.database import init_db
 
 
 
-app= FastAPI(title= "Booking API")
-
-
-
-
-
-@app.lifespan("startup")
-async def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     await init_db()
+    yield
 
+
+app= FastAPI(title= "Booking API", lifespan= lifespan)
