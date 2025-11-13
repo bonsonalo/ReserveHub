a97a89bd9ce103f2fba5105c8ce2689e1621d919
bonsonalo <bonson2468@gmail.com>
2025-11-13 18:21:50 +0800

    feat: add response pydantic schema for user reonse

--- backend/app/schema/user_schema.py
+++ backend/app/schema/user_schema.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, EmailStr
 from typing import Optional
 from sqlalchemy import UUID
 import uuid
-
+from datetime import date
 
 
 
@@ -11,3 +11,13 @@ class UpdateUser(BaseModel):
     full_name: Optional[str]
     phone: Optional[str]
 
+
+class UserResponse(BaseModel):
+    email: EmailStr
+    full_name: str
+    phone: str
+    created_at: date
+    updated_at: date
+    archived: bool
+    role: str
+    
