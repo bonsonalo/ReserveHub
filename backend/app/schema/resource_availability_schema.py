ca2fc07dcb24f5189a9b45e36f6108295f485cb8
bonsonalo <bonson2468@gmail.com>
2025-11-15 23:20:48 +0800

    feat: add createresourceavailability schema

--- backend/app/schema/resource_availability_schema.py
+++ backend/app/schema/resource_availability_schema.py
@@ -1,7 +1,7 @@
+from typing import Any, Optional
 from pydantic import BaseModel
 from datetime import time, date
-
-
+from uuid import UUID
 
 class AvailabilityTime(BaseModel):
     start_time: time
@@ -10,3 +10,15 @@ class AvailabilityTime(BaseModel):
 
     class Config:
         from_attribute= True    # allows SQLAlchemy models to be converted automatically
+
+
+class CreateResourceAvailability(BaseModel):
+    resource_id: UUID
+    recurrence: list[dict[str, Any]]
+    start_date: date
+    end_date: date
+    start_time: time
+    end_time: time
+    tz: Optional[str]= "UTC"
+    is_exception: bool
+    
