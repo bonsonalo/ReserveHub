f9dfae2cbcc623da631fde59a0fbc81e3b4eb605
bonsonalo <bonson2468@gmail.com>
2025-11-19 23:14:17 +0800

    fix: fix the range as date_range in the for  loop and  return True after the time conflict which was previously False unintentionally

--- backend/app/utils/scheduling.py
+++ backend/app/utils/scheduling.py
@@ -14,12 +14,12 @@ def date_range(start, end):
 def has_conflict(updated, existing_availabilities, existing_booking):
     def check_blocks(blocks):
         for block in blocks:
-            for d1 in range(updated.start_date, updated.end_date):
-                for d2 in range(block.start_date, block.end_date):
+            for d1 in date_range(updated.start_date, updated.end_date):
+                for d2 in date_range(block.start_date, block.end_date):
                     if d1 != d2:
                         continue
                     if updated.start_time < block.end_time and block.start_time < updated.end_time:
-                        return False
+                        return True
         return False
     
     return (
