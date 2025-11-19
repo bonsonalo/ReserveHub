baa5d3d692834115b50e6cd5417dc627e0811d5c
bonsonalo <bonson2468@gmail.com>
2025-11-19 14:49:49 +0800

    feat: add scheduling function that checks the overlap of time between the updated resource availabilities with existing availabilities and exisiting booking

---
+++ backend/app/utils/scheduling.py
@@ -0,0 +1,28 @@
+from datetime import timedelta
+
+
+
+
+def date_range(start, end):
+    current= start
+
+    while current <= end:
+        yield current
+        current+= timedelta(days= 1) 
+
+
+def has_conflict(updated, existing_availabilities, existing_booking):
+    def check_blocks(blocks):
+        for block in blocks:
+            for d1 in range(updated.start_date, updated.end_date):
+                for d2 in range(block.start_date, block.end_date):
+                    if d1 != d2:
+                        continue
+                    if updated.start_time < block.end_time and block.start_time < updated.end_time:
+                        return False
+        return False
+    
+    return (
+        check_blocks(existing_availabilities) or 
+        check_blocks(existing_booking)
+    )
