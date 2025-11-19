351b10ba334c6e9b7fd1912e7c0b71ba75b49fcd
bonsonalo <bonson2468@gmail.com>
2025-11-19 23:10:16 +0800

    feat: add patch service layer for resource_availability

--- backend/app/service/resource_availability_service.py
+++ backend/app/service/resource_availability_service.py
@@ -7,9 +7,10 @@ from uuid import UUID
 from backend.app.model.booking import Booking
 from backend.app.model.resource import Resource
 from backend.app.model.resource_availability import ResourceAvailability
-from backend.app.schema.resource_availability_schema import CreateResourceAvailability
+from backend.app.schema.resource_availability_schema import CreateResourceAvailability, UpdateAvailabilityRequest
 from backend.app.utils.resource_exist import resource_exist
 from backend.app.core.logger import logger
+from backend.app.utils.scheduling import has_conflict
 
 
 
@@ -62,3 +63,45 @@ async def create_resource_availability_service(resource_id: UUID, info: CreateRe
     await db.commit()
     await db.refresh(db_add)
     return db_add
+
+
+async def patch_resource_availability_by_id_service(id: UUID, updated_data: UpdateAvailabilityRequest, db: AsyncSession):
+    #fetch availability
+    availability= await db.scalar(select(ResourceAvailability).where(ResourceAvailability.id == id))
+    resource_exist(availability)
+    
+
+    #list all existint resource availabilies of the same resource excluding the one we are updating
+    existing_availabilities= await db.scalars(select(ResourceAvailability).where(
+        ResourceAvailability.resource_id == availability.resource_id,
+        ResourceAvailability.id != availability.id
+    )).all()
+
+    # load the existing bookings for that particular resource
+
+    existing_booking= await db.scalar(select(Booking).where(Booking.resource_id == availability.resource_id)).all()
+
+
+    if has_conflict(updated_data, existing_availabilities, existing_booking):
+        raise FileExistsError
+    
+    if updated_data.start_date is not None:
+        availability.start_date = updated_data.start_date
+    if updated_data.end_date is not None:
+        availability.end_date = updated_data.end_date
+    if updated_data.start_time is not None:
+        availability.start_time = updated_data.start_time
+    if updated_data.end_time is not None:
+        availability.end_time = updated_data.end_time
+    if updated_data.tz is not None:
+        availability.tz = updated_data.tz
+    if updated_data.is_exception is not None:
+        availability.is_exception = updated_data.is_exception
+
+    await db.commit()
+    await db.refresh(availability)
+
+    return availability
+
+
+
