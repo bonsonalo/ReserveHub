a687d3717964cf89ffb96c4f228040e847207b7d
bonsonalo <bonson2468@gmail.com>
2025-11-21 11:59:32 +0800

    feat: add get_all endpoint for booking

--- backend/app/api/v1/endpoints/bookings.py
+++ backend/app/api/v1/endpoints/bookings.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from starlette import status
-
+from uuid import UUID
 
 from backend.app.schema.booking_schema import CreateBooking
 from backend.app.core.config import db_dependency, admin_dependency, user_dependency, superadmin_dependency
-from backend.app.service.booking_service import create_booking_service
+from backend.app.service.booking_service import create_booking_service, get_booking_service
 from backend.app.utils.authentication_check import authentication_check
 from backend.app.core.logger import logger
 
@@ -29,3 +29,16 @@ async def create_booking(to_create: CreateBooking, current_user: admin_dependenc
     except ValueError as e:
         logger.error(str(e))
         HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= str(e))
+
+
+# GET /api/v1/bookings
+
+@router.get("/get_all")
+async def get_bookings(current_user: user_dependency, db: db_dependency, user_id: UUID= Query(None), resource_id: UUID= Query(None), booking_status: str= Query(None)):
+    authentication_check(current_user)
+
+    try:
+        return await get_booking_service(current_user, db, user_id, resource_id, booking_status)
+    except ValueError as e:
+        logger.erro(str(e))
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= str(e))
