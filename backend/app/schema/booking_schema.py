9f7f43755e067ee07bdb03b65e62b341545a0dba
bonsonalo <bonson2468@gmail.com>
2025-11-22 16:59:59 +0800

    feat: create RescheduleBooking schema and add start and end time in createBooking schema and create_by for the

--- backend/app/schema/booking_schema.py
+++ backend/app/schema/booking_schema.py
@@ -1,29 +1,33 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from sqlalchemy import Enum as sqlEnum
 from sqlalchemy.dialects.postgresql import TSTZRANGE, JSONB
 from enum import Enum
 from uuid import UUID
-
+from datetime import datetime
 class BookingStatus(str, sqlEnum):
     booked= "booked"
     cancelled= "cancelled"
     deleted= "deleted"
 
-
-
 class CreateBooking(BaseModel):
     resource_id: UUID
     user_id: UUID
     status: Optional[str]= None
-    time_range: TSTZRANGE
-    attendees: Optional[int]= None
+    start_time: datetime
+    end_time: datetime
+    attendees: Optional[int]= Field(None, ge= 1)
     data: Optional[dict]= None
     is_recurring: Optional[bool]= None
-    recurrence_rule: str
+    recurrence_rule: Optional[str]= None
+    created_by: UUID
 
 
 class UpdateRequest(BaseModel):
     status: Optional[str]= None
     attendees: Optional[int]= None
-    data: Optional[JSONB]= None
+    data: Optional[dict]= None
+
+class RescheduleBooking(BaseModel):
+    start_time: datetime
+    end_time: datetime
