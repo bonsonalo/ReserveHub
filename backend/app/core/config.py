0dcf2f8752513d1cbe11900607cf8c8b4f84badd
bonsonalo <bonson2468@gmail.com>
2025-11-22 16:57:41 +0800

    fix: replace the iint with UUID for user_id

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,7 +6,7 @@ import os
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 from starlette import status
-
+from uuid import UUID
 
 from backend.app.core.database import AsyncSessionLocal
 from backend.app.core.logger import logger
@@ -34,7 +34,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth_bearer)]):
     try:
         payload= jwt.decode(token, SECRET_KEY, algorithms= [ALGORITHM])
         email:str= payload.get("sub")
-        user_id: int= payload.get("id")
+        user_id: UUID= payload.get("id")
         role: str= payload.get("role")
 
         if email is None or user_id is None or role is None:
