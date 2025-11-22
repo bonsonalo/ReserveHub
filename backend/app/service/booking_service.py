b17b355a0d17ddb2f032615a70120d7aa087d5bf
bonsonalo <bonson2468@gmail.com>
2025-11-22 16:33:19 +0800

    feat: add tstzrange for the time range and fix error in using current_user["id"] instead of current_user.id

--- backend/app/service/booking_service.py
+++ backend/app/service/booking_service.py
@@ -1,6 +1,9 @@
+from sqlalchemy import func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Optional
 from uuid import UUID
+from sqlalchemy.dialects.postgresql import TSTZRANGE
+
 
 from backend.app.model.booking import Booking
 from backend.app.schema.booking_schema import CreateBooking, UpdateRequest
@@ -8,17 +11,22 @@ from backend.app.utils.resource_exist import resource_exist
 
 
 
-
 async def create_booking_service(to_create: CreateBooking, db: AsyncSession):
+
+    time_range= func.tstzrange(
+        to_create.start_time,
+        to_create.end_time
+    )
     db_add= Booking(
         resource_id= to_create.resource_id,
         user_id= to_create.user_id,
-        status= to_create.user_id,
-        time_range= to_create.time_range,
+        status= to_create.status,
+        time_range= time_range,
         attendees= to_create.attendees,
         data= to_create.data,
         is_recurring= to_create.is_recurring,
-        recurrence_rule= to_create.recurrence_rule
+        recurrence_rule= to_create.recurrence_rule,
+        created_by= to_create.created_by
     )
 
     db.add(db_add)
@@ -31,9 +39,9 @@ async def create_booking_service(to_create: CreateBooking, db: AsyncSession):
 
 async def get_booking_service(current_user, db: AsyncSession, user_id: UUID | None, resource_id: UUID | None, booking_status: str):
 
-    current_id= current_user.id
-    current_role= current_user.role
-    query= db.select(Booking)
+    current_id= current_user["id"]
+    current_role= current_user["role"]
+    query= select(Booking)
 
     if current_role not in ["admin", "superadmin"]:
         query = query.where(Booking.user_id == current_id)
@@ -48,10 +56,10 @@ async def get_booking_service(current_user, db: AsyncSession, user_id: UUID | No
     return result.all()
 
 async def get_booking_by_id_service(booking_id: UUID, current_user, db: AsyncSession):
-    current_role= current_user.role
-    current_id= current_user.id
+    current_role= current_user["role"]
+    current_id= current_user["id"]
 
-    query= db.select(Booking)
+    query= select(Booking)
 
     if current_role not in ["admin", "superadmin"]:
         query= query.where((Booking.user_id == current_id) & (Booking.id == booking_id))
@@ -59,12 +67,12 @@ async def get_booking_by_id_service(booking_id: UUID, current_user, db: AsyncSes
         query = query.where(Booking.id == booking_id)
 
     result = await db.scalars(query)
-    return await result.all()
+    return result.all()
 
 
 
 async def update_booking_service(booking_id: UUID, to_update: UpdateRequest, db: AsyncSession):
-    query= await db.scalar(Booking).where(Booking.id== booking_id)
+    query= await db.scalar(select(Booking).where(Booking.id== booking_id))
     resource_exist(query)
 
     if to_update.attendees is not None:
