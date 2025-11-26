502d346cbd474a20ea787c6937190a55cc5f5d06
bonsonalo <bonson2468@gmail.com>
2025-11-26 16:37:23 +0800

    feat: add custom middleware

---
+++ backend/app/core/middleware.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+import time
+
+
+
+
+async def register_middleware(app: FastAPI):
+    
+
+    @app.middleware("http")
+    async def custom_logging(request: Request, call_next):
+        start_time= time.time()
+
+        response = await call_next(request)
+        processing_time= time.time() - start_time
+
+        message= f"{request.client.host} : {request.client.port} - {request.method} - {request.url.path} - {response.status_code} completed after {processing_time}"
+
+        print(message)
+        return response
+
+    
+                       
+
