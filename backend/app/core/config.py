18000e2f7c0d77a54ce0837ecae56765961ac87b
bonsonalo <bonson2468@gmail.com>
2025-12-05 22:47:44 +0800

    feat: add role_required in admin dependency which was mistakenly deleted

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -64,7 +64,7 @@ def role_required(allowed_roles: List[str]):
 
 
 user_dependency= Annotated[dict, Depends(role_required(["user", "admin", "superadmin"]))]
-admin_dependency= Annotated[dict, Depends((["admin", "superadmin"]))]
+admin_dependency= Annotated[dict, Depends(role_required(["admin", "superadmin"]))]
 superadmin_dependency= Annotated[dict, Depends(role_required(["superadmin"]))]
 
 MAIL_PASS = os.getenv("MAIL_PASS")
