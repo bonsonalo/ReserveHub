e9a3755dc26ac2cb0b7ab8beaf5e52a715dff99a
bonsonalo <bonson2468@gmail.com>
2025-12-05 16:48:54 +0800

    feat: add email service

---
+++ backend/app/service/email_service.py
@@ -0,0 +1,23 @@
+from fastapi_mail import FastMail, MessageSchema, MessageType
+from backend.app.core.config import conf
+from pydantic import EmailStr
+
+
+fm = FastMail(conf)
+
+
+async def send_welcome_email(recipient_email: list[EmailStr]):
+
+    html= """ 
+    <h1> welcome</h1>
+    <p>your accounthas been created successfully</p>
+    """
+
+    message= MessageSchema(
+        subject= "Welcome to our platform",
+        recipients= [recipient_email],
+        body= "Heyyyy",
+        subtype= html
+    )
+
+    await fm.send_message(message)
