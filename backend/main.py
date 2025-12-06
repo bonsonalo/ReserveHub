93ab45ad882628a6e66955ac843c81b4d517ba38
bonsonalo <bonson2468@gmail.com>
2025-12-07 01:28:54 +0800

    feat: add redis in the initialization

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager 
 from mangum import Mangum
+from redis import Redis
+import httpx
 
 from backend.app.core.database import init_db
 from backend.app.api.v1.routes import routers
@@ -10,7 +12,10 @@ from backend.app.core.middleware import register_middleware
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await init_db()
+    app.state.redis= Redis(host= 'localhost', port= 6379)
+    app.state.http_client= httpx.AsyncClient()
     yield
+    app.state.redis.close()
 
 
 app= FastAPI(title= "Booking API", lifespan= lifespan)
