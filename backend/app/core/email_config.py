4f758cd76e67932212fb429c2da2c7e44e36423b
bonsonalo <bonson2468@gmail.com>
2025-12-08 13:25:02 +0800

    feat: add email config

---
+++ backend/app/core/email_config.py
@@ -0,0 +1,21 @@
+from fastapi_mail import ConnectionConfig
+import os
+
+
+
+
+MAIL_PASS = os.getenv("MAIL_PASS")
+MAIL_USERNAME= os.getenv("MAIL_USERNAME")
+
+conf= ConnectionConfig(
+    MAIL_USERNAME= MAIL_USERNAME,
+    MAIL_PASSWORD= MAIL_PASS,
+    MAIL_FROM= "bonson2468@gmail.com",
+    MAIL_PORT= "2525",
+    MAIL_SERVER= "sandbox.smtp.mailtrap.io",
+    MAIL_FROM_NAME= "my_app",
+    MAIL_STARTTLS= True,
+    MAIL_SSL_TLS= False,
+    USE_CREDENTIALS= True,
+    VALIDATE_CERTS= True
+    )
