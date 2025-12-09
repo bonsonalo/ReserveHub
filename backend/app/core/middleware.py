06b762ba7c6c6aabb226f9f110c9cf69f9416bf4
bonsonalo <bonson2468@gmail.com>
2025-12-09 23:58:00 +0800

    feat: add rate limiting middleware

--- backend/app/core/middleware.py
+++ backend/app/core/middleware.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, Request
+from collections import defaultdict
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 import time
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
-
-
+from starlette.middleware.base import BaseHTTPMiddleware
+from typing import Dict
 
 async def register_middleware(app: FastAPI):
     
@@ -35,5 +36,51 @@ async def register_middleware(app: FastAPI):
                        
                        
     
-                       
+class AdvancedMiddleware(BaseHTTPMiddleware):
+    
+    def __init__(self, app):
+        super().__init__(app)
+        self.rate_limit_records: Dict[str, float]= defaultdict(float)
+    async def log_message(self, message: str):
+        print(message)
+    async def dispatch(self, request: Request, call_next):
+        client_ip= request.client.host
+        current_time= time.time()
+
+        if current_time- self.rate_limit_records[client_ip] < 1: # 1 request per second limit
+            return Response(content="rate limit exceeded", status_code=429)
+        
+        self.rate_limit_records[client_ip]= current_time
+
+        path= request.url.path
+        await self.log_message(f"request to {path}")
+
+        #process sthe request
+
+        start_time= time.time()
+        response= await call_next(request)
+        process_time= time.time() - start_time
+
+
+        #add custom header without modifying he original headers object
+
+        custom_headers= {"X-Process-Time": str(process_time)}
+        for header, value in custom_headers.items():
+            response.headers.append(header, value)
+
+
+        #asynchronous logging for processing time
+
+        await self.log_message(f"response for {path} took {process_time} seconds")
+
+        return response
+    
+    
+
+        
+
+
+
+
+
 
